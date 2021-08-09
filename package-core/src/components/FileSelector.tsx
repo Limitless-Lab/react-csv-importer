cb96f1b0e17a457b09c21990fb312aa9193fec4f
Carl Joven <carljoven626@gmail.com>
2021-08-09 13:04:58 +0800

    feat: :sparkles: Parse excel file

--- package-core/src/components/FileSelector.tsx
+++ package-core/src/components/FileSelector.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useRef } from 'react';
 import { useDropzone } from 'react-dropzone';
+import XLSX from 'xlsx';
 
 import './FileSelector.scss';
 
@@ -16,7 +17,28 @@ export const FileSelector: React.FC<{ onSelected: (file: File) => void }> = ({
     }
 
     const file = acceptedFiles[0];
-    onSelectedRef.current(file);
+
+    if (file.name.includes('.xls')) {
+      const reader = new FileReader();
+      reader.onload = (evt) => {
+        const data = new Uint8Array(evt.target?.result as never);
+        const workbook = XLSX.read(data, { type: 'array' });
+
+        const converted = XLSX.utils.sheet_to_csv(
+          workbook.Sheets[workbook.SheetNames[0]]
+        );
+
+        const blob = new Blob([converted], { type: 'text/csv' });
+
+        const newFile = new File([blob], file.name);
+
+        onSelectedRef.current(newFile);
+      };
+
+      reader.readAsArrayBuffer(file);
+    } else {
+      onSelectedRef.current(file);
+    }
   }, []);
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
@@ -32,9 +54,11 @@ export const FileSelector: React.FC<{ onSelected: (file: File) => void }> = ({
       <input {...getInputProps()} />
 
       {isDragActive ? (
-        <span>Drop CSV file here...</span>
+        <span>Drop CSV or Excel file here...</span>
       ) : (
-        <span>Drag-and-drop CSV file here, or click to select in folder</span>
+        <span>
+          Drag-and-drop CSV or Excel file here, or click to select in folder
+        </span>
       )}
     </div>
   );
